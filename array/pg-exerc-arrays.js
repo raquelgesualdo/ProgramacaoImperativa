033d5a595a7a8221ce922b77ff3606e237062f3c
raquelgesualdo <raquel.gesualdo@gmail.com>
2021-06-11 12:09:04 -0300

    incluindo comentário

--- array/pg-exerc-arrays.js
+++ array/pg-exerc-arrays.js
@@ -1,3 +1,12 @@
+/*
+Crie um array que contenha nomes de produtos para compra.
+Após isso, exiba no console os resultados das funções relacionadas aos arrays, que são: Join, Pop, Push, Shift e Unshift.
+Também se deve escrever com suas palavras, o que cada função realiza.
+Seguem abaixo alguns exemplos.
+console.log(“O método Join realiza tal coisa”)
+console.log(RESULTADO_DO_JOIN)
+*/
+
 let lista = ["leite","cafe","achocolatado"];
 
 //join

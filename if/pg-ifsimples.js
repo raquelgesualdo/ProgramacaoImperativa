06ba3fff90db2a9ac4a5ff29f75e2d78e3f3c145
raquelgesualdo <raquel.gesualdo@gmail.com>
2021-06-11 02:15:49 -0300

    Exercícios Aula 4

---
+++ if/pg-ifsimples.js
@@ -0,0 +1,14 @@
+/*Com este exercício, vamos construir um if simples.
+Declare uma variável com o nome status. Atribua a ela um valor booleano: (true ou false).
+Em seguida, faça um if que avalie o valor contido nesta variável. Se o valor armazenado for true, imprima a seguinte mensagem no console: O valor é true - verdadeiro. Caso contrário, imprima a mensagem: O valor é false - falso.
+*/
+
+let status = true
+
+if(status == true){
+    console.log("O valor é true - verdadeiro");
+}
+    
+if(status == false){
+    console.log("O valor é false - falso");
+}

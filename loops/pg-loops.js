d6ae1e711c65c2c92e8ac9152769885dca141210
raquelgesualdo <raquel.gesualdo@gmail.com>
2021-06-13 23:45:39 -0300

    assincrono loops

---
+++ loops/pg-loops.js
@@ -0,0 +1,16 @@
+// entendendo ciclos - estrutura básica
+//for(inicio;condicao;modificador){}
+    //contador;limite;modificador do contador
+    //código que será executado em cada repetição.
+
+for(let volta=1;volta<=5;volta++){
+    console.log("Volta número: " +volta);
+};
+
+for(let i=1;i<=10;i++){
+    console.log(i*7);
+};
+
+for(let i=0;i<=4;i++){
+    console.log("oi");
+};

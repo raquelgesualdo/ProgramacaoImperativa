89f9fbbd7163377da2f6541b4e4c2b1578188d39
raquelgesualdo <raquel.gesualdo@gmail.com>
2021-06-24 13:53:29 -0300

    inserindo comentários

--- modulos/assincrono1.js
+++ modulos/assincrono1.js
@@ -1,3 +1,5 @@
+//Crie um módulo que exporte uma função (em um arquivo separado). Após isso, importe o módulo no arquivo principal e execute a função importada.
+
 const imoveis = ["Casa de Veraneio", "Casa da Praia", "Apartamento Escritório", "Moradia fixa"];
 
 module.exports = imoveis;
--- modulos/assincrono2.js
+++ modulos/assincrono2.js
@@ -1,3 +1,5 @@
+//Crie um módulo que exporte uma função (em um arquivo separado). Após isso, importe o módulo no arquivo principal e execute a função importada.
+
 const imoveis = require("./assincrono1");
 
 console.log(imoveis);

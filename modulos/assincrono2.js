431bd765bae88f4424adbdfccdd7716bb1b80fc3
raquelgesualdo <raquel.gesualdo@gmail.com>
2021-06-24 14:25:13 -0300

    mais comentário

--- modulos/assincrono2.js
+++ modulos/assincrono2.js
@@ -1,4 +1,5 @@
 //Crie um módulo que exporte uma função (em um arquivo separado). Após isso, importe o módulo no arquivo principal e execute a função importada.
+// o funcao original no caso está no arquivo assincrono1.js
 
 const imoveis = require("./assincrono1");
 
